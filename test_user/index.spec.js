b726bec1cacc5a767a323e54bab56a343a3f840f
Sakarin Juajan <maxzasakarin@gmail.com>
2021-09-21 20:49:51 +0700

    GET user for Homework2

--- test_user/index.spec.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest')
-const app = require('../users/index')
-
-describe('GET /users', () => {
-    it('responds with "No user in the system"', (done) => {
-        request(app).get('/users').expect('No user in the system', done)
-    })
-})
